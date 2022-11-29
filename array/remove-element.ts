0b2fd3baa02978019c68ccc09ae79f5428077257
wanyakun <yakun.wan@gmail.com>
2022-11-29 12:08:36 +0800

    添加 去除数组重复项

---
+++ array/remove-element.ts
@@ -0,0 +1,12 @@
+export function removeElement(nums: number[], val: number): number {
+  let slow = 0
+  let fast = 0
+  while (fast < nums.length) {
+    if (nums[fast] !== val) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+    fast++
+  }
+  return slow
+}

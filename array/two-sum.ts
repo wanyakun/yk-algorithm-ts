c11f20f94ca963a63cd3bf95e5b4876f7abd14d6
wanyakun <yakun.wan@gmail.com>
2022-11-29 11:37:56 +0800

    怎么两数之和II

---
+++ array/two-sum.ts
@@ -0,0 +1,16 @@
+export function twoSum(numbers: number[], target: number): number[] {
+  let left = 0
+  let right = numbers.length - 1
+  while(left < right) {
+    if (numbers[left] + numbers[right] === target) {
+      // 下标从1开始
+      return [left + 1, right + 1]
+    } else if (numbers[left] + numbers[right] < target) {
+      // 小于目标值，让左侧变大
+      left++
+    } else if (numbers[left] + numbers[right] > target) {
+      right++
+    }
+  }
+  return [-1, -1]
+}

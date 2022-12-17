1cedc930634c477460a5c18eedb8bd5d91c1eb8e
wanyakun <yakun.wan@gmail.com>
2022-12-18 00:36:50 +0800

    翻转二叉树

---
+++ tree/invert-binary-tree.ts
@@ -0,0 +1,36 @@
+import { TreeNode } from "../data-struct/tree-node";
+
+// 迭代遍历
+export function invertTree(root: TreeNode | null): TreeNode | null {
+  traverse(root)
+  return root
+}
+
+function traverse(node: TreeNode | null) {
+  if (node === null) {
+    return
+  }
+
+  let temp = node.left
+  node.left = node.right
+  node.right = temp
+
+  traverse(node.left)
+  traverse(node.right)
+}
+
+// 递归
+
+export function invertTree1(root: TreeNode | null): TreeNode | null {
+  if (root === null) {
+    return null
+  }
+
+  let left = invertTree(root.left)
+  let right = invertTree(root.right)
+
+  root.left = right
+  root.right = left
+
+  return root
+}
